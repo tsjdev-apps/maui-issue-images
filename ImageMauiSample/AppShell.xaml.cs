383630168d73d2391c297f2773cd5b027a5e0af6
Sebastian Jensen <apps@tsjdev-apps.de>
2023-08-29 10:39:45 +0200

    Add sample project.

---
+++ ImageMauiSample/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ImageMauiSample
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ImageMauiSample/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ImageMauiSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
